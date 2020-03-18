d88b6fa6586c5b7542f4ce02c6c4f1d2bd346dfd
DeadneM <deadnem@gmail.com>
2020-03-18 21:22:41 +0100

    separate keybind because folder-master make keybind disapear

--- Hooks/menu.lua
+++ Hooks/menu.lua
@@ -109,31 +109,7 @@ Hooks:Add("MenuManagerInitialize", "StatTrak_MenuManagerInitialize", function(me
 		StatTrak._data.mode_se_dmg = (item:value() == "on" and true or false)
 	end	
 	MenuCallbackHandler.stattrak_change_mode = function(self, item)
-		if Utils:IsInHeist() then
-			StatTrak.mode = StatTrak.mode + 1
-			if StatTrak.mode > #StatTrak.used_modes then
-				StatTrak.mode = 1
-			end
-			if not DelayedCalls or not DelayedCalls._calls then return end
-			if #StatTrak.used_modes > 0 then
-				StatTrak.force_text = managers.localization:text("st_mode_" .. StatTrak.used_modes[StatTrak.mode])
-			else
-				StatTrak.force_text = managers.localization:text("st_mode_empty")
-			end
-			local function refresh()
-				StatTrak.force_text = nil
-				StatTrak:update_screen()
-			end
-			
-			for k, v in pairs(DelayedCalls._calls) do
-				if v.stattrak then
-					DelayedCalls._calls[k] = nil
-				end
-			end
-			DelayedCalls:Add("StatTrak_show_mode", 1, refresh)
-			DelayedCalls._calls["StatTrak_show_mode"].stattrak = true
-			StatTrak:update_screen()			
-		end
+	--Do nothing as we set the keybind in mod.txt for folder structure
 	end
 	StatTrak:Load()
 	MenuHelper:LoadFromJsonFile(StatTrak._menu_file, StatTrak, StatTrak._data)
